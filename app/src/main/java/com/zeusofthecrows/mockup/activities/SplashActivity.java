98022f5cff06bb0a703a5067541428d16d654ffb
ZeusOfTheCrows <zeusofthecrows+github@gmail.com>
2020-04-11 15:56:28 +0100

    Should now run; and package name should be correct

---
+++ app/src/main/java/com/zeusofthecrows/mockup/activities/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.zeusofthecrows.mockup.activities;
+
+import android.support.annotation.NonNull;
+
+import com.dm.material.dashboard.candybar.activities.CandyBarSplashActivity;
+import com.dm.material.dashboard.candybar.activities.configurations.SplashScreenConfiguration;
+import com.zeusofthecrows.mockup.R;
+
+public class SplashActivity extends CandyBarSplashActivity {
+
+    @NonNull
+    @Override
+    public SplashScreenConfiguration onInit() {
+        return new SplashScreenConfiguration(MainActivity.class)
+                .setBottomText(getString(R.string.splash_screen_title));
+    }
+}
